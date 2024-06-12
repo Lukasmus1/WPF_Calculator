d660e178e2050c4088231bee0538b693f77d8c65
Lukáš Píšek <lukin.pisek@email.cz>
2024-06-13 00:55:13 +0200

    chore: started work on localization
    problem with including Texts subfolder located in Resources folder

--- WPF_Calculator/WPF_Calculator/App.xaml.cs
+++ WPF_Calculator/WPF_Calculator/App.xaml.cs
@@ -9,4 +9,16 @@ namespace WPF_Calculator;
 /// </summary>
 public partial class App : Application
 {
+    void App_Startup(object sender, StartupEventArgs e)
+    {
+        MainWindow mainWindow = new MainWindow();
+        SetHeightAndWidth(mainWindow);
+        mainWindow.Show();
+    }
+    
+    private static void SetHeightAndWidth(Window window)
+    {
+        window.Height = 0.5 * SystemParameters.PrimaryScreenHeight;
+        window.Width = 0.2 * SystemParameters.PrimaryScreenWidth;
+    }
 }
